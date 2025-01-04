b4ed648a7622fab171bf945730c676489a298f97
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2025-01-04 07:53:01 +0100

    refactor ⚙:  Converted js frontend to ts

---
+++ src/dao-forge-frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export const App = () => {
+    return (
+        <div>
+        <h1>DAO Forge</h1>
+        <p>DAO Forge is a platform for creating and managing DAOs.</p>
+        </div>
+    );
+}
