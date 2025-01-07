840764cd090d5fe091348a6d0d2c0def9c496b50
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2025-01-07 14:42:39 +0100

    feat 🦄:  Added script to deploy only the needed canisters

---
+++ scripts/deploy-canisters.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Script directory: $SCRIPT_DIR"
+
+cd $SCRIPT_DIR/..
+
+# Create canisters
+dfx canister create --all
+
+# Build canisters
+dfx build
+
+# Deploy canisters
+dfx deploy dao-forge-backend
+dfx deploy dao-forge-frontend
