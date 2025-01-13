e2408746e7a71c042826ed7b6c50303ace088674
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2025-01-13 09:10:38 +0100

    feat 🦄:  Added the possibility to take the params from the call inside the dao_manager

--- src/dao-manager/src/controller/dao_controller.rs
+++ src/dao-manager/src/controller/dao_controller.rs
@@ -1,41 +1,14 @@
 use crate::services::CanisterManagementService;
-use candid::{encode_args, CandidType, Principal};
-
-// FIXME: Think a better solution to handle the init args
-#[derive(CandidType)]
-struct InitArgs {
-    pub name: String,
-    pub address: String,
-    pub zip: u32,
-    pub town: String,
-    pub uid: String,
-    pub ch_id: String,
-    pub frc_id: u64,
-    pub purpose: String,
-    pub board: Vec<String>,
-    pub members: Vec<String>,
-}
+use crate::types::DaoAssociationInitArgs;
+use candid::{encode_args, Principal};
 
 #[ic_cdk::update]
-async fn create_dao() -> Result<Principal, String> {
+async fn create_dao_association(params: DaoAssociationInitArgs) -> Result<Principal, String> {
     let wasm =
         include_bytes!("../../../../target/wasm32-unknown-unknown/release/dao_association.wasm")
             .to_vec();
 
-    let init_args = InitArgs {
-        name: "My DAO".to_string(),
-        address: "My Address".to_string(),
-        zip: 12345,
-        town: "My Town".to_string(),
-        uid: "My UID".to_string(),
-        ch_id: "My CH ID".to_string(),
-        frc_id: 123,
-        purpose: "My Purpose".to_string(),
-        board: vec!["board1".to_string(), "board2".to_string()],
-        members: vec!["member1".to_string(), "member2".to_string()],
-    };
-
-    let encoded_args = encode_args((init_args,)).unwrap();
+    let encoded_args = encode_args((params,)).unwrap();
 
     CanisterManagementService::deploy_canister(wasm, encoded_args, vec![ic_cdk::api::id()]).await
 }
--- src/dao-manager/src/lib.rs
+++ src/dao-manager/src/lib.rs
@@ -1,2 +1,3 @@
 mod controller;
 mod services;
+mod types;
